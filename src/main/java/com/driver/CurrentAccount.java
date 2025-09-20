b7c01363e98c2fab2f6b812db526fcaccbd0bf6a
Pramod-D-K <pramoddk8590@gmail.com>
2025-09-21 00:51:07 +0530

    Banking

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,19 +1,47 @@
 package com.driver;
 
+import java.util.Collections;
+import java.util.Random;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name,balance,5000);
+        this.tradeLicenseId=tradeLicenseId;
     }
 
+    public  boolean checkIsValid(String upperCaseId){
+        boolean isValid=true;
+        for(int i = 1; i< upperCaseId.length(); i++){
+            if(upperCaseId.charAt(i)==upperCaseId.charAt(i-1)){
+                isValid=false;
+            }
+        }
+        return isValid;
+    }
     public void validateLicenseId() throws Exception {
-        // A trade license Id is said to be valid if no two consecutive characters are same
-        // If the license Id is valid, do nothing
-        // If the characters of the license Id can be rearranged to create any valid license Id
+        // A trade license id is said to be valid if no two consecutive characters are same
+        // If the license id is valid, do nothing
+        // If the characters of the license id can be rearranged to create any valid license id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
-    }
 
+        Random random= new Random();
+        String id = random.toString();
+        String upperCaseId = id.toUpperCase();
+        boolean isValid=checkIsValid(upperCaseId);
+        if(isValid){
+            return;
+        }
+        Collections.shuffle(Collections.singletonList(upperCaseId));
+        isValid=checkIsValid(upperCaseId);
+        if(!isValid){
+            throw new Exception("Valid License can not be generated");
+        }
+        else{
+            return;
+        }
+    }
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -6,23 +6,40 @@ public class SavingsAccount extends BankAccount{
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
+        super(name,balance,0);
+        this.maxWithdrawalLimit=maxWithdrawalLimit;
+        this.rate=rate;
 
     }
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-
+        if(amount>this.maxWithdrawalLimit){
+            throw new Exception("Maximum Withdraw Limit Exceed");
+        }
+        super.withdraw(amount);
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
+        if(super.getBalance()<=0){
+            return super.getBalance();
+        }
+        double simpleInterest=super.getBalance()/100*years*this.rate;
+        return simpleInterest+super.getBalance();
 
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+        if(super.getBalance()<=0){
+            return super.getBalance();
+        }
+        int tn=times*years;
+        //double d1= 1+(this.rate/times)/100;
+        double d= Math.pow(1+(this.rate/times)/100,tn);
+        return super.getBalance()*d;
     }
 
 }
