a6a6fff011236894cab2844a8b39278e2d60e64f
Pramod-D-K <pramoddk8590@gmail.com>
2025-09-21 00:58:02 +0530

    No compilation error

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -4,10 +4,10 @@ public class StudentAccount extends BankAccount{
 
     private String  institutionName;
 
-    public StudentAccount(String name, double balance, String  institutionName) {
+    public StudentAccount(String name, double balance, String institutionName1) {
         //minimum balance is 0 by default
         super(name,balance,0);
-        this.institutionName=institutionName;
+        this.institutionName=institutionName1;
     }
 
 }
